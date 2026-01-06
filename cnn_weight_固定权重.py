b697a2ccda737275e8738c76f5c8a92a93e93e9d
evol <56601096@qq.com>
2026-01-06 17:16:28 +0800

    全部代码修改

--- cnn_weight_固定权重.py
+++ cnn_weight_固定权重.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import pandas as pd
 from PIL import Image
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from tqdm import tqdm
 
 # 自动检测计算设备
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -249,6 +250,13 @@ def train_net(model, ema_model, lr, num_epochs, train_loader, val_loader,class_w
         # 训练过程
         model.train()
         total_loss = 0.0
+        progress_bar = tqdm(
+            train_loader,
+            desc=f"Epoch [{epoch + 1}/{num_epochs}]",
+            leave=False
+        )
+        correct_count = 0
+        total_count = 0
         for images, labels in train_loader:
             images, labels = images.to(DEVICE), labels.to(DEVICE)
             # 前向传播
@@ -260,7 +268,12 @@ def train_net(model, ema_model, lr, num_epochs, train_loader, val_loader,class_w
             loss.backward()
             optimizer.step()
             #累加损失
+            preds = logits.argmax(dim=1)
+            total_count += labels.numel()
+            correct_count += (preds == labels).sum().item()
             total_loss += loss.item()
+            
+            progress_bar.set_postfix(loss=loss.item())
         print(f'第{epoch + 1}次循环:')
 
         # 训练损失统计
@@ -269,7 +282,7 @@ def train_net(model, ema_model, lr, num_epochs, train_loader, val_loader,class_w
         print(f"\t训练平均loss为{avg_loss}")
 
         # 训练准确率统计
-        train_acc = evaluate(model, train_loader)
+        train_acc = correct_count/total_count
         list_train_acc.append(train_acc)
         print(f'\t训练正确率为{train_acc}')
 
